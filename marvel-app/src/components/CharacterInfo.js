84e9c90d18ebb9e580a6fd056dc61bbcdf49309f
Cleyra Uzcategui <cleyra.uzcategui@gmail.com>
2018-09-13 20:51:52 +0100

    Added Container info and displayed name, image and top 20 comics

---
+++ marvel-app/src/components/CharacterInfo.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Character from './Character';
+
+const CharacterInfo = (props) => {
+  if(!props.character){
+    return null;
+  }
+
+  return (<React.Fragment>
+           <p>Name: {props.character.name}</p>
+           <img src={props.character.thumbnail.path +"."+props.character.thumbnail.extension} className = "image" alt="Character image"
+           width="300" height="400"/>
+           <p>Bio: {props.character.description}</p>
+           <p>Top 20 comics </p>
+           {
+             displayComics(props.character.comics.items)
+           }
+        </React.Fragment>
+        )
+}
+
+function displayComics(comics) {
+    return comics.map(function (comic) {
+        return (
+            <div className="row">
+                <div className="col-md-1">
+                    {comic.name}
+                </div>
+            </div>
+        )
+    })
+}
+
+export default CharacterInfo;
--- marvel-app/src/containers/CharacterByIdContainer.js
+++ marvel-app/src/containers/CharacterByIdContainer.js
@@ -1,23 +1,49 @@
 import React, {Component} from 'react';
 import CryptoJS  from 'crypto-js';
+import CharacterInfo from '../components/CharacterInfo';
+
+const PRIVATE_KEY = "e5ebb5751fbadc8b568517a79f2aa494e22f4357";
+const PUBLIC_KEY = "e5de7dbf97acd76b7f887d6ab69a1075";
+const API_BASE_URL = 'https://gateway.marvel.com/v1/public/';
 
 class CharacterByIdContainer extends Component{
 
     constructor(props){
       super(props)
-    if(props.params.id){ //from the path 'course/:id')        
-      console.log("Getting Character ID"+props.params.id);
-    }  
-     
-  }
+      this.state = {
+        character:[]
+      };
+      let id = props.params.id;
+    if(id){
+      this.loadAPIFunction(id);
+    }
+  };
+
+  loadAPIFunction(id){
+    console.log("Calling API");
+    let url = API_BASE_URL + 'characters/' + id + '?' + this.getHash();
+    console.log(url);
+    fetch(url)
+    .then(responseText => responseText.json())
+    .then(characters => this.setState({character: characters.data.results}))
+    .catch(err => console.log(err));
+  };
+
+getHash() {
+      let timestamp = new Date().getTime();
+      let md5 = CryptoJS.MD5(timestamp + PRIVATE_KEY + PUBLIC_KEY).toString();
+      return "ts=" + timestamp + "&apikey=" + PUBLIC_KEY + "&hash=" + md5;
+    };
 
   render(){
     return(
+      <React.Fragment>
      <p> Character Info </p>
+     <CharacterInfo character={this.state.character[0]} />
+   </React.Fragment>
     )
   }
- 
- };
- 
- export default CharacterByIdContainer;
 
+ };
+
+ export default CharacterByIdContainer;
